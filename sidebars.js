5658ce1cf2f8c376777f3c81bcbba4604932518a
revital <revital@allegro.ai>
2025-02-23 13:56:12 +0200

    Replace "Applications" ToC item with use-cases

--- sidebars.js
+++ sidebars.js
@@ -92,25 +92,17 @@ module.exports = {
             {type: 'ref', id: 'webapp/applications/apps_jupyter_lab'},
             {type: 'ref', id: 'webapp/applications/apps_vscode'}
         ]},
-        {
-                    type: 'category',
-                    collapsible: true,
-                    collapsed: true,
-                    label: 'ClearML Applications',
-                    link: {type: 'doc', id: 'webapp/applications/apps_overview'},
-                    items: [
-                        {"General": [
-
-                            'webapp/applications/apps_dashboard',
-                            'webapp/applications/apps_task_scheduler',
-                            'webapp/applications/apps_trigger_manager',
-                        ]},
-                        {"UI Dev": [
-                            'webapp/applications/apps_gradio',
-                            'webapp/applications/apps_streamlit'
-                        ]},
-                    ]
-                },
+        {"Building Interactive Model Interfaces": [
+            {type: 'ref', id: 'webapp/applications/apps_gradio'},
+            {type: 'ref', id: 'webapp/applications/apps_streamlit'},
+        ]},
+        {"Automating Task Execution": [
+            {type: 'ref', id: 'webapp/applications/apps_task_scheduler'},
+            {type: 'ref', id: 'webapp/applications/apps_trigger_manager'},
+        ]},
+        {"Monitoring Project Progress": [
+            {type: 'ref', id: 'webapp/applications/apps_dashboard'},
+        ]},
     ],
     integrationsSidebar: [
             {
